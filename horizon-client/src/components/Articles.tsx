785d706b10df089ec9ff38f70546ea5b37235ef9
Ross O'Reilly <oreillyross@gmail.com>
2019-03-05 13:14:59 +0000

    set noImplicitAny to false

--- horizon-client/src/components/Articles.tsx
+++ horizon-client/src/components/Articles.tsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import {List} from 'react-virtualized'
 
+const list = [
+  "One item",
+  "Two Item",
+  "three Item"
+]
+
+function rowRenderer ({
+  key,         // Unique key within array of rows
+  index,       // Index of row within collection
+  isScrolling, // The List is currently being scrolled
+  isVisible,   // This row is visible within the List (eg it is not an overscanned row)
+  style        // Style object to be applied to row (to position it)
+}) {
+  return (
+    <div
+      key={key}
+      style={style}
+    >
+      {list[index]}
+    </div>
+  )
+}
  
 
 const Articles = () => (
   <div>
-  React Virtualized should go here
+  <List
+    height={480}
+    width={640}
+    rowHeight={20}
+    rowCount={list.length}
+    rowRenderer={rowRenderer}
+  />
+  
   </div>
 );
 
